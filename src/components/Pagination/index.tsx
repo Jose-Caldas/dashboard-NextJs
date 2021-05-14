3672f68c022a97c7e048f447f967aafa080a30ab
José Caldas <jrcaldas@yahoo.com.br>
2021-05-14 12:07:12 -0300

    use prefetch

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -62,12 +62,29 @@ export function Pagination({
         )}
         {previousPages.length > 0 &&
           previousPages.map((page) => {
-            return <PaginationItem key={page} number={page} />;
+            return (
+              <PaginationItem
+                onPageChange={onPageChange}
+                key={page}
+                number={page}
+              />
+            );
           })}
-        <PaginationItem number={currentPage} isCurrent />;
+        <PaginationItem
+          onPageChange={onPageChange}
+          number={currentPage}
+          isCurrent
+        />
+        ;
         {nextPages.length > 0 &&
           nextPages.map((page) => {
-            return <PaginationItem key={page} number={page} />;
+            return (
+              <PaginationItem
+                onPageChange={onPageChange}
+                key={page}
+                number={page}
+              />
+            );
           })}
         {currentPage + siblingsCount < lastPage && (
           <>
@@ -76,7 +93,7 @@ export function Pagination({
                 ...
               </Text>
             )}
-            <PaginationItem number={lastPage} />
+            <PaginationItem onPageChange={onPageChange} number={lastPage} />
           </>
         )}
       </Stack>
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -8,8 +8,19 @@ type User = {
   created_at: string;
 };
 
-export async function getUsers(): Promise<User[]> {
-  const { data } = await api.get("users");
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+};
+
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get("users", {
+    params: {
+      page,
+    },
+  });
+
+  const totalCount = Number(headers["x-total-count"]);
 
   const users = data.users.map((user) => {
     return {
@@ -24,14 +35,17 @@ export async function getUsers(): Promise<User[]> {
     };
   });
 
-  return users;
+  return {
+    users,
+    totalCount,
+  };
 }
 
-export function useUsers() {
+export function useUsers(page: number) {
   return useQuery(
-    "users",
-    getUsers,
+    ["users", page],
+    () => getUsers(page),
 
-    { staleTime: 1000 * 5 } // 5 segundos
+    { staleTime: 1000 * 60 * 10 } // 10 minutos
   );
 }
